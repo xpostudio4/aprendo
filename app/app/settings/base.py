f692535b5df1c5bc645bb9dc40e47cbb79a94c77
Leonardo Jimenez <ljimenez@stancedata.com>
2014-10-02 20:16:05 -0400

    added admins names and emails

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -33,7 +33,8 @@ TEMPLATE_DEBUG = DEBUG
 ########## MANAGER CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#admins
 ADMINS = (
-    ('Your Name', 'your_email@example.com'),
+    ('Leonardo Jimenez', 'ljimenez@stancedata.com'),
+    ('Vivian Guillen', 'vguilen@stancedata.com'),
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#managers
